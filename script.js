4473ff41021c506ec5c620308f532f55597f5f62
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-22 17:40:29 +0200

    Bonus esercizio 1

--- script.js
+++ script.js
@@ -4,26 +4,36 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-let word = prompt('Inserisci una parola');
-// Creo una funzione
-function isPalindrome(word) {
-    // Creo una variabile che contenga la risposta finale
-    let answer = '';
+const textField = document.getElementById('text');
+const form = document.querySelector('form');
+const resultElement = document.getElementById('result');
 
-    // Creo un ciclo for per ciclare in ogni lettera della parola inserita dall'utente
-    for (let i = 0; i < word.length; i++) {
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-        // Creo un if per comparare le lettere
-        if (word.charAt(0) === word.charAt(word.length -1)){
-            answer = `La parola ${word} è palindroma`
-        } else{
-            answer = `La parola ${word} non è palindroma`
+    // Raccolta dati
+    let word = textField.value.trim();
+    // Creo una funzione
+    function isPalindrome(word) {
+        // Creo una variabile che contenga la risposta finale
+        let answer = '';
+
+        // Creo un ciclo for per ciclare in ogni lettera della parola inserita dall'utente
+        for (let i = 0; i < word.length; i++) {
+
+            // Creo un if per comparare le lettere
+            if (word.charAt(0) === word.charAt(word.length - 1)) {
+                answer = `La parola ${word} è palindroma`;
+            } else {
+                answer = `La parola ${word} non è palindroma`;
+            }
         }
+        // Preparo la risposta
+        return answer;
     }
-    // Preparo la risposta
-    return answer;
-}
- // Stampo la risposta fuori dalla funzione
-console.log(isPalindrome(word))
+    // Stampo la risposta fuori dalla funzione
+    let result = (isPalindrome(word));
 
+    resultElement.innerHTML = result;
+})
 
